344fc92275f5e9b8d71caca2347a9782cb63c8f8
kmikiya <K.mikiya97@gmail.com>
2022-02-17 11:00:39 +0000

    a

--- db/migrate/20220118042940_create_illusts.rb
+++ db/migrate/20220118042940_create_illusts.rb
@@ -1,7 +1,7 @@
 class CreateIllusts < ActiveRecord::Migration[5.2]
   def change
     create_table :illusts do |t|
-      t.string :title
+      t.string :titlerail
       t.string :image
 
       t.timestamps
